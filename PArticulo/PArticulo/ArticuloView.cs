d5ff4573a770c1a619eae487d9a464bf2c8365e5
esterdam <esther@pcI5-02.(none)>
2012-12-12 10:26:15 +0100

    Actualizando PArticulo con el comboBoxCategoria

--- PArticulo/PArticulo/ArticuloView.cs
+++ PArticulo/PArticulo/ArticuloView.cs
@@ -3,6 +3,7 @@ using Serpis.Ad;
 using PAdSerpis;
 using System;
 using System.Data;
+using Npgsql;
 
 namespace PArticulo
 {
@@ -11,26 +12,61 @@ namespace PArticulo
 		public ArticuloView (long id) : base(Gtk.WindowType.Toplevel)
 		{
 			this.Build ();
-
+			
+			object comboBoxValue;
+			
 			IDbCommand dbCommand = ApplicationContext.Instance.DbConnection.CreateCommand();
 			dbCommand.CommandText = string.Format ("select * from articulos where id={0}", id);
+			
+			//Llenar opciones comboBox
+			ListStore listStore = new ListStore(typeof(string));
+			
+			IDbCommand dbCommand1 = ApplicationContext.Instance.DbConnection.CreateCommand();
+			dbCommand1.CommandText = string.Format ("select nombre from categoria");
+			
+			IDataReader dataReader1 = dbCommand1.ExecuteReader ();
+			
+			while(dataReader1.Read()) {
+				listStore.AppendValues((string)dataReader1["nombre"]);
+			}
+		
+			comboBoxCategoria.Model = listStore;
+		
+			dataReader1.Close();
+			//Fin opciones comboBox
 			
 			IDataReader dataReader = dbCommand.ExecuteReader ();
 			dataReader.Read ();
 			
 			entryNombre.Text = (string)dataReader["nombre"];
 			spinButtonPrecio.Value = Convert.ToDouble( (int)dataReader["precio"] );
+			Int64 categoria = (Int64)dataReader["categoria"];
 			
 			dataReader.Close ();
+			
+			comboBoxCategoria.Changed += delegate {
+				TreeIter treeIter;
+				if(comboBoxCategoria.GetActiveIter (out treeIter)) { //Item seleccionado
+					comboBoxValue = listStore.GetValue(treeIter, 0);
+					
+					dbCommand1.CommandText = string.Format ("select id from categoria where nombre='{0}'", comboBoxValue.ToString());
+					dataReader1 = dbCommand1.ExecuteReader ();
+					
+					dataReader1.Read();
+					categoria = (Int64)dataReader1["id"];
+					dataReader1.Close();
+				}
+			};
 			
 			saveAction.Activated += delegate {
 				Console.WriteLine("saveAction.Activated");
 				
 				IDbCommand dbUpdateCommand = ApplicationContext.Instance.DbConnection.CreateCommand ();
-				dbUpdateCommand.CommandText = "update articulos set nombre=:nombre, precio=:precio where id=:id";
+				dbUpdateCommand.CommandText = "update articulos set nombre=:nombre, precio=:precio, categoria=:categoria where id=:id";
 				
 				DbCommandExtensions.AddParameter (dbUpdateCommand, "nombre", entryNombre.Text);
 				DbCommandExtensions.AddParameter (dbUpdateCommand, "precio", Convert.ToInt32 (spinButtonPrecio.Value ));
+				DbCommandExtensions.AddParameter (dbUpdateCommand, "categoria", categoria );
 				DbCommandExtensions.AddParameter (dbUpdateCommand, "id", id);
 	
 				dbUpdateCommand.ExecuteNonQuery ();
@@ -38,7 +74,7 @@ namespace PArticulo
 				Destroy ();
 			};
 		}
-	
+		
 	}
 }
 
